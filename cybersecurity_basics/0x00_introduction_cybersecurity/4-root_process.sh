3f4ac9fa11c3f6654118659640f051884c7358c9
AkbarX05 <13619@holbertonstudents.com>
2026-05-07 17:18:06 +0400

    Update 4-root_process.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-# 1-ci arqument olaraq istifadəçi adının daxil edilib-edilmədiyini yoxlayırıq
-if [ -z "$1" ]; then
-    echo "İstifadə: $0 <username>"
-    exit 1
-fi
-
-USER=$1
-
-# ps komandası ilə prosesləri siyahalayırıq:
-# -u: Spesifik istifadəçi üçün
-# aux: Detallı siyahı üçün
-# grep -v: VSZ (5-ci sütun) və RSS (6-cı sütun) dəyəri 0 olanları çıxarır
-ps aux | grep "^$USER" | grep -v ' 0     0 '
+ps -u "$1" -F | grep -v " 0      0 "
