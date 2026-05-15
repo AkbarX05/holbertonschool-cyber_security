a02dfab623f6cef044a2c2a935216f1ef0c0143a
AkbarX05 <13619@holbertonstudents.com>
2026-05-15 14:20:06 +0400

    Update 9-firewall.sh

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -A INPUT -p tcp -s "$1" --dport 22 -j ACCEPT
-iptables -P INPUT DROP
+iptables -P INPUT -j DROP && iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
